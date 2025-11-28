fe733658e2c25db5e92be38a9986ea4e4cdd659b
ANdrey Baranov <randebar@gmail.com>
2025-11-29 01:05:25 +0300

    update of logic

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,11 +5,12 @@ import "fmt"
 func main() {
 	const USD_TO_EUR = 0.86
 	const USD_TO_RUB = 77.48
+	const EURO_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
 	var amount float64
 	var rub float64
 	fmt.Print("Введите количство евро: ")
 	fmt.Scan(&amount)
-	rub = (amount / USD_TO_EUR) * USD_TO_RUB
+	rub = (amount * EURO_TO_RUB)
 	fmt.Printf("%.2f EURO is %.2f RUB\n", amount, rub)
 }
