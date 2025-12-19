b8500ec350c12e98b35f2f7653cc3647203096e4
ANdrey Baranov <randebar@gmail.com>
2025-12-19 03:00:31 +0300

    append the new logic

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,10 +7,22 @@ func main() {
 	const USD_TO_RUB = 77.48
 	const EURO_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
-	var amount float64
 	var rub float64
-	fmt.Print("Введите количство евро: ")
-	fmt.Scan(&amount)
+	amount := getTheCurrencyValues()
 	rub = (amount * EURO_TO_RUB)
 	fmt.Printf("%.2f EURO is %.2f RUB\n", amount, rub)
+}
+
+/*
+Get the euro amount
+*/
+func getTheCurrencyValues() (amount float64) {
+	fmt.Print("Введите количество евро: ")
+	fmt.Scan(&amount)
+
+	return
+}
+
+func convertTheCurrency(value float64, sourceCurrency string, destCurrency string) {
+	// some logic
 }
