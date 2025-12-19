8554dc68213e67d304e1c83fcfaa6bed7e8b9b23
ANdrey Baranov <randebar@gmail.com>
2025-12-19 03:51:53 +0300

    update the logic

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,28 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const USD_TO_EUR = 0.86
+const USD_TO_RUB = 77.48
+const EURO_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
 func main() {
-	const USD_TO_EUR = 0.86
-	const USD_TO_RUB = 77.48
-	const EURO_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
-	var rub float64
-	amount := getTheCurrencyValues()
-	rub = (amount * EURO_TO_RUB)
-	fmt.Printf("%.2f EURO is %.2f RUB\n", amount, rub)
+	var sourceCurrency string
+	var destCurrency string
+	var result float64
+	var amount float64
+
+	sourceCurrency, amount, destCurrency, error := getTheCurrencyValues()
+	if error != nil {
+		fmt.Printf("%s\n", error)
+		return
+	}
+	result, error = convertTheCurrency(amount, sourceCurrency, destCurrency)
+	if error != nil {
+		fmt.Printf("%s\n", error)
+		return
+	}
+	fmt.Printf("%.2f %s is %.2f %s\n", amount, sourceCurrency, result, destCurrency)
 }
 
 /*
 Get the euro amount
 */
-func getTheCurrencyValues() (amount float64) {
-	fmt.Print("Введите количество евро: ")
+func getTheCurrencyValues() (string, float64, string, error) {
+	var sourceCurrency string
+	var destCurrency string
+	var amount float64
+	fmt.Print("Введите исходную валюту (USD, RUB, EURO): ")
+	fmt.Scan(&sourceCurrency)
+	if sourceCurrency != "USD" && sourceCurrency != "RUB" && sourceCurrency != "EURO" {
+		return "", 0, "", errors.New("Используйте только предложенные варианты валют")
+	}
+	fmt.Print("Введите желаемую валюту (USD, RUB, EURO): ")
+	fmt.Scan(&destCurrency)
+	if destCurrency != sourceCurrency {
+		if destCurrency != "USD" && destCurrency != "RUB" && destCurrency != "EURO" {
+			return "", 0, "", errors.New("Используйте только предложенные варианты валют")
+		}
+	} else {
+		return "", 0, "", errors.New("Используйте только предложенные варианты валют (исключая уже введенную)")
+	}
+	fmt.Printf("Введите количество %s: ", sourceCurrency)
 	fmt.Scan(&amount)
+	if amount < 0 {
+		return "", 0, "", errors.New("Значение не может быть меньше нуля")
+	}
 
-	return
+	return sourceCurrency, amount, destCurrency, nil
 }
 
-func convertTheCurrency(value float64, sourceCurrency string, destCurrency string) {
-	// some logic
+func convertTheCurrency(value float64, sourceCurrency string, destCurrency string) (float64, error) {
+	if sourceCurrency == destCurrency {
+		return 0, errors.New("Валюта должна различаться")
+	}
+
+	if sourceCurrency == "USD" && destCurrency == "RUB" {
+		return value * USD_TO_RUB, nil
+	}
+	if sourceCurrency == "EURO" && destCurrency == "RUB" {
+		return value * EURO_TO_RUB, nil
+	}
+	if sourceCurrency == "USD" && destCurrency == "EURO" {
+		return value / USD_TO_EUR, nil
+	}
+	if sourceCurrency == "RUB" && destCurrency == "EURO" {
+		return value / USD_TO_RUB, nil
+	}
+	if sourceCurrency == "RUB" && destCurrency == "USD" {
+		return value / USD_TO_RUB, nil
+	}
+	return 0, errors.New("Неизвестная ошибка")
 }
